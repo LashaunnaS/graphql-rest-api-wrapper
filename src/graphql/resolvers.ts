0a42cf26423cf4a9eadc72b499581069645a19fa
LashaunnaS <shaunna191@icloud.com>
2021-04-21 22:14:47 +0200

    Update resolvers to account for fetching color from pokemon species.

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,13 +1,22 @@
-import { PokemonDefaultData } from '../generated/graphql';
+import { Pokemon, PokemonSpecies } from '../generated/graphql';
 
 const resolvers = {
-  Query: {
-    getPokemonDefaultData: async (
+  Pokemon: {
+    pokemonSpecies: async (
       _source: unknown,
       { id }: { id: string },
       { dataSources }: { dataSources: any },
-    ): Promise<PokemonDefaultData> => {
-      return dataSources.pokeAPI.getPokemonDefaultData(id);
+    ): Promise<PokemonSpecies> => {
+      return dataSources.pokeAPI.getPokemonSpeciesData(id);
+    },
+  },
+  Query: {
+    getPokemon: async (
+      _source: unknown,
+      { id }: { id: string },
+      { dataSources }: { dataSources: any },
+    ): Promise<Pokemon> => {
+      return dataSources.pokeAPI.getPokemon(id);
     },
   },
 };
